a1f695f6c761e4649c66649fb8fe1c66d1a5680d
rof <romain.fournier26@gmail.com>
2020-12-09 11:08:01 +0100

    permanent api key

--- trajet.py
+++ trajet.py
@@ -7,7 +7,7 @@ from math import floor
 import numpy as np
 import pandas as pd
 
-api_key="2J403ttwc2LRE-aOkUUz8vfrDoCuun1iiLROfaWA5Ew"
+api_key="uMo8ZVdrrdwrdMUAttOx6W0ms4cTCp0oS-EypFl3gNQ"
 
 def read_file(filename, nrows=None):
     data = pd.read_csv(filename, sep="\t", names=["user", "timestamp", "long", "lat"], nrows=nrows)
@@ -30,6 +30,7 @@ def get_distance(ptA, ptB):
 def getPath(departure, arrival): ## pts are (lat , lon)
 #get a route from here API
     route = requests.get("https://router.hereapi.com/v8/routes?transportMode=pedestrian&origin="+str(departure[0])+","+str(departure[1])+"&destination="+str(arrival[0])+","+str(arrival[1])+"&return=polyline&apiKey="+api_key)
+    print(route)
     path = fp.decode((route.json()["routes"][0]["sections"][0]["polyline"]))
     # re-sample the polyline 
     
@@ -65,14 +66,15 @@ if __name__ == "__main__":
     
     ptA = ( 43.409484, 3.687372) ## lat and long of pt A
     ptB = ( 43.409461, 3.687383) ## lat and long of pt B
+
+    '''
     minLat = min(ptA[0], ptB[0])
     minLong = min(ptA[1], ptB[1])
     maxLat = max(ptA[0], ptB[0])
     maxLong  = max(ptA[1], ptB[1])
-    print(minLat)
     iter_csv = pd.read_csv('./privamov/privamov-gps', sep="\t", names=["user", "timestamp", "long", "lat"], nrows=1000, iterator=True, chunksize=10)
     df = pd.concat([chunk[filter_rows(chunk, minLat, minLong, maxLat, maxLong)] for chunk in iter_csv])
-    
-    print(df)
+    '''
+    print(getPath(ptA, ptB))
    
     
